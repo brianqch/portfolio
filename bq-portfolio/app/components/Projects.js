2eb91c53a7555c7ac2f2f1984ad1608bc9c11447
Brian Quach <brianquach@berkeley.edu>
2024-08-25 13:58:52 -0700

    Added Code Comment Generator project

--- bq-portfolio/app/components/Projects.js
+++ bq-portfolio/app/components/Projects.js
@@ -15,7 +15,7 @@ export default function Projects() {
                             <Image
 
                                 src="/cookbookai_image.png"
-                                alt="Description of the image"
+                                alt="Screenshot of Cookbook.AI project image"
                                 loading="lazy"
                                 width={200}
                                 height={48}
@@ -55,7 +55,7 @@ export default function Projects() {
                             <Image
 
                                 src="/seedling_image.png"
-                                alt="Description of the image"
+                                alt="Screnshot of Seedling Project"
                                 loading="lazy"
                                 width={200}
                                 height={48}
@@ -93,7 +93,7 @@ export default function Projects() {
                             <Image
 
                                 src="/clothsimulator_image.png"
-                                alt="Description of the image"
+                                alt="Screenshot of Cloth Simulator project"
                                 loading="lazy"
                                 width={200}
                                 height={48}
@@ -117,6 +117,41 @@ export default function Projects() {
                                 </p>
                                 <ul className="mt-2 flex flex-wrap">
                                     <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-[#518ee0] px-3 py-1 text-xs font-medium leading-5 text-white">C++</div></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li className="mb-12">
+                        <div className="group relative grid gap-4 pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 bg-[#222222] lg:block lg:group-hover:bg-[#333333] lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg">
+
+                            </div>
+                            {/* <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-200 sm:col-span-2">July 2022 - May 2023</header> */}
+                            <Image
+
+                                src="/codecomment_image.png"
+                                alt="Screenshot of Code Comment Project"
+                                loading="lazy"
+                                width={200}
+                                height={48}
+                                className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 sm:order-1 sm:col-span-2 sm:translate-y-1"
+                            />
+                            <div className="z-10 sm:order-2 sm:col-span-6">
+                                <h3 className="font-medium leading-snug text-slate-200">
+                                    <div>
+                                        <span className="inline-flex flex-col items-baseline font-medium leading-tight  hover:text-[#518ee0] focus-visible:text-[#518ee0] group/link text-base">
+                                            <span className="text-slate-200">AI Code Comment Generator</span>
+                                        </span>
+                                        {/* <span class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span> */}
+                                    </div>
+                                </h3>
+                                <p className="mt-2 text-sm leading-normal text-slate-200">
+                                    • Developed an AI-driven code comment generator using Next.js, TypeScript, and Cohere's GenAI, automating the process of creating context-aware code comments to improve code readability and maintainability.
+                                </p>
+                                <ul className="mt-2 flex flex-wrap">
+                                <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-[#518ee0] px-3 py-1 text-xs font-medium leading-5 text-white">Next.js</div></li>
+                                    <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-[#518ee0] px-3 py-1 text-xs font-medium leading-5 text-white">TypeScript</div></li>
                                 </ul>
                             </div>
                         </div>
